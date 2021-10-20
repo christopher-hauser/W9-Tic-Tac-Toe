02611544022d9aaec60ec5e55792e967c265d90f
Christopher Hauser <chauser6@gmail.com>
2021-10-20 17:59:18 -0400

    first space working

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,1 +1,20 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", e => {
+    // Track the clicks of the players
+    const X = document.createElement("img");
+    X.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg"
+    const O = document.createElement("img");
+    O.src = "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg"
+    let turnCount = 1;
+
+    const space1 = document.getElementById("space1")
+    space1.addEventListener("click", e => {
+        if (turnCount % 2 === 1 && ) {
+            space1.style = ("background-image: url('https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg')")
+        } else if (turnCount % 2 === 0) {
+            space1.style = ("background-image: url('https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg')")
+        } else{
+            break;
+        }
+        turnCount++;
+    })
+})
